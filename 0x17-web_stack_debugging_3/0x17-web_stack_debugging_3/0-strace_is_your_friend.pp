3fe7a4c3f8bce4ee30661af350873fd3de409366
Onwunzo Amara Peace <101415207+amarapeace@users.noreply.github.com>
2022-12-08 21:25:19 +0100

    Delete 0x17-web_stack_debugging_3/0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# debug faulty wordpress site
-exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-/var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
-}
